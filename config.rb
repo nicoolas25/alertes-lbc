c02bd7763fbafccb2261197b340597efeac3259b
Nicolas ZERMATI <nicoolas25@gmail.com>
2012-06-26 21:12:49 +0200

    Deployement issues

--- config.rb
+++ config.rb
@@ -1,4 +1,4 @@
-RACK_ENV     = ENV['RACK_ENV'] || 'development'
+RACK_ENV = ENV['RACK_ENV'] || 'development'
 
 if RACK_ENV != 'heroku'
   FILEPATH     = File.expand_path(File.dirname(__FILE__))
@@ -7,16 +7,17 @@ if RACK_ENV != 'heroku'
   DB           = Sequel.connect(CONFIG_DB[RACK_ENV].to_sym)
 else
   CONFIG_EMAIL = {
-    'smtp' => {
-      'address' => ENV['EMAIL_SMTP_SERVER']           || 'smtp.gmail.com',
-      'port' => ENV['EMAIL_SMTP_PORT']                || '587',
-      'enable_starttls_auto' => ENV['EMAIL_SMTP_TLS'] || true,
-      'authentication' => ENV['EMAIL_SMTP_AUTH']      || 'login',
-      'domain' => ENV['EMAIL_SMTP_DOMAIN']            || 'localhost.localdomain',
-      'user_name' => ENV['EMAIL_SMTP_USER'],
-      'password' => ENV['EMAIL_SMTP_PASS']},
-    'email' => {
-      'to' => ENV['EMAIL_TARGET']}}
+    'heroku' => {
+      'smtp' => {
+        'address' => ENV['EMAIL_SMTP_SERVER']           || 'smtp.gmail.com',
+        'port' => ENV['EMAIL_SMTP_PORT']                || '587',
+        'enable_starttls_auto' => ENV['EMAIL_SMTP_TLS'] || true,
+        'authentication' => ENV['EMAIL_SMTP_AUTH']      || 'login',
+        'domain' => ENV['EMAIL_SMTP_DOMAIN']            || 'localhost.localdomain',
+        'user_name' => ENV['EMAIL_SMTP_USER'],
+        'password' => ENV['EMAIL_SMTP_PASS']},
+      'email' => {
+        'to' => ENV['EMAIL_TARGET']}}}
 
   DB = Sequel.connect(ENV['DATABASE_URL'])
 end
